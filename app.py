e71740f05e57ee45f9d492e0f1c73cacd857fa5a
garbados <garbados@gmail.com>
2012-09-18 14:20:55 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from Bio import SeqIO
+import yaml, random, sys
+
+## CONFIG ##
+with file("config.yml", "r") as f:
+    config = yaml.load(f.read())
+try:
+    dialect = config[sys.argv[1]]
+except Exception as e:
+    print e.message
+    dialect = config['bro']
+
+## Reads the first ten characters from a random genetic sequence
+bio_data = [seq_record.seq.tostring() for seq_record in SeqIO.parse("bio/lady_slippers_orchid.fasta", "fasta")]
+to_speak = list(bio_data[random.choice(range(len(bio_data)))])
+
+for letter, word in dialect.iteritems():
+    to_speak = [char if char.lower() != letter else word for char in to_speak]
+
+to_speak = " ".join(to_speak[:10])
+print to_speak
---
+++ speak.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+python app.py $1 | espeak -s 120 -v us-mbrola-1 -p 99
